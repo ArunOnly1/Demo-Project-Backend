3ad0fe07fb7781edc555535a153e38d80f912226
ArunOnly1 <erarun1nly1@gmail.com>
2022-02-04 12:13:07 +0545

    v1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://arunchapagain-ebpearls.netlify.app/',
+      'https://arunchapagain-ebpearls.netlify.app',
     ],
     credentials: true,
     exposedHeaders: ['refreshtoken', 'accesstoken'],
