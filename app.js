c37612f7ba3e242c072868d9421dbf8846dd633a
ArunOnly1 <erarun1nly1@gmail.com>
2022-02-04 12:33:05 +0545

    v1

--- app.js
+++ app.js
@@ -25,7 +25,25 @@ app.use(cookieParser())
 //   })
 // )
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', 'http://shikshya.crews.draftserver.com:5025');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Expose-Headers', 'accessToken, refreshToken,')
+    res.header(
+      'Access-Control-Allow-Methods',
+      'PUT, POST, PATCH, DELETE, GET, OPTIONS'
+    )
+    return res.status(200).json({})
+  }
+
+  return next()
+})
 db_connect()
 
 app.use((req, res, next) => {
